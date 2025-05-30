c0070c4ff53eeb319bfd3885762ac4c0d687a0fa
Inglauraarteagarojas <120149449+Inglauraarteagarojas@users.noreply.github.com>
2025-05-30 11:44:09 -0500

    app.py

--- app.py
+++ app.py
@@ -209,3 +209,4 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
     debug_mode = os.environ.get('FLASK_ENV') == 'development'
     app.run(host='0.0.0.0', port=port, debug=debug_mode)
+    
